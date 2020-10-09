9641cd62aca28366e8e7e65e366c4d4148597354
Luca <lucakanyo@protonmail.com>
2020-10-09 16:15:18 -0400

    wrapped navlinks to unordered list and li tags

--- time-client/src/App.js
+++ time-client/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
+import { Route, BrowserRouter as Router} from 'react-router-dom'
 
 class  App extends Component {
   render(){
   return (
     <div className="App">
       HI. THIS IS APP Component 
+      <Router>
+
       <Navbar/>
+      </Router>
     </div>
   );
   }
--- time-client/src/components/Navbar.js
+++ time-client/src/components/Navbar.js
@@ -4,7 +4,14 @@ import {Link, NavLink} from 'react-router-dom'
 const Navbar = () => {
     return (
         <nav className="nav-wrapper indigo accent-1">
-            <div className="container"></div>
+            <div className="container">
+                <ul>
+
+                <li><NavLink exact to="/">HOMEBOI</NavLink></li>
+                <li><NavLink to="/about">ABOUT</NavLink></li>
+                <li><NavLink to="/contact">CONTACT</NavLink></li>
+                </ul>
+            </div>
             
         </nav>
     )
