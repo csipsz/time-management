9ee6b3ee54f2bd492378e91fd9981873c6604bec
Luca <lucakanyo@protonmail.com>
2020-10-09 15:58:13 -0400

    added materialize link to html and Navbar

--- time-client/src/App.js
+++ time-client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar'
 
 class  App extends Component {
   render(){
   return (
     <div className="App">
-      HI
+      HI. THIS IS APP Component 
+      <Navbar/>
     </div>
   );
   }
---
+++ time-client/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <nav className="nav-wrapper indigo accent-1">
+            <div className="container"></div>
+            
+        </nav>
+    )
+}
+
+export default Navbar
+
