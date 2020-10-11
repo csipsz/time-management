c09ef42af090882b507dfeb7fb2a3f565de3026e
Luca <lucakanyo@protonmail.com>
2020-10-11 15:30:27 -0400

    added more seed data

--- time-client/src/components/TaskCard.js
+++ time-client/src/components/TaskCard.js
@@ -5,7 +5,7 @@ const TaskCard = (props) => {
 
     return (
         <div className="task-card pink darken-4">
-          <button onClick={() => props.delete(props.task)} className="right btn blue lighten-2">X</button>
+          <button onClick={() => props.delete(props.task)} className="right btn red lighten-2">X</button>
           <button id={props.task.id} onClick={(e) => props.completion(e.target)} className={props.task.completed ? "right btn green lighten-2" :"right btn blue lighten-2" }>✓</button>
           <b>{props.task.title}</b><br/>
           <b>{props.task.day}</b>
