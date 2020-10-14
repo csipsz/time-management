7d84d2b22bb8df2a761bf075785135561aa422a9
Luca <lucakanyo@protonmail.com>
2020-10-14 13:10:32 -0400

    added rails custom validation

--- time-management-api/app/models/task.rb
+++ time-management-api/app/models/task.rb
@@ -1,4 +1,16 @@
 class Task < ApplicationRecord
     validates :title, presence: true
     validates :day, presence: true
+
+    validate :no_madeup_days
+    private 
+
+    def no_madeup_days 
+        days = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"]
+
+        if !days.include? self.day.upcase 
+            self.errors.add(:day, " must exist")
+        end 
+    end 
+
 end
