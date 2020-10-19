9c775a3606a2da406cee412e3f8da330273a97d9
Luca <lucakanyo@protonmail.com>
2020-10-19 17:53:30 -0400

    added defaultprops to taskcard component

--- time-client/src/components/TaskCard.js
+++ time-client/src/components/TaskCard.js
@@ -9,10 +9,14 @@ const TaskCard = (props) => {
           <button onClick={() => props.delete(props.task)} className="right btn red">X</button>
           <button id={props.task.id} onClick={(e) => props.completion(e.target)} className={props.task.completed ? "right btn green" :"right btn blue lighten-2" }>✓</button>
           <p onClick={e => props.history.push(`tasks/${props.task.id}`)}><b>{props.task.title}</b><br/></p>
-          <p>{props.task.description}</p>
+          <p>{props.task.description || props.description}</p>
           <img className="react-logo" src={ReactLogo} alt="react"/>
         </div>
     )
+}
+
+TaskCard.defaultProps = {
+  description: "you could've add a description ya know"
 }
 
 export default withRouter(TaskCard)
